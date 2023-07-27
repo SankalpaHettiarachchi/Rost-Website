afb9b1218af3f602d388ac60191132d1effdc617
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-07-28 04:47:24 +0530

    Album Front end ongoing

--- resources/views/site/albums.blade.php
+++ resources/views/site/albums.blade.php
@@ -12,12 +12,14 @@
             @foreach ($albums as $albums_item )
                 <div class="col col-md-4">
                     <div class="grid">
-                        <div class="entry-media">
-                            @foreach ($albums_item->images->take(3) as $image_item)
-                                <img src="{{ asset('uploads/'. $image_item->image_name) }}" alt class="img img-responsive">
-                             @endforeach
-                        </div>
                         <div class="entry-details">
+                            <div class="gallery">
+                                    @foreach ($albums_item->images->take(6) as $index => $image_item)
+                                        <div class="col col-3 box" tabindex="{{ $index + 1 }}">
+                                            <img src="{{ asset('uploads/' . $image_item->image_name) }}" alt="">
+                                        </div>
+                                    @endforeach
+                            </div>
                             <h3><a href="#">{{ isset($albums) ? $albums_item->album_name : '' }}</a></h3>
                             <span class="entry-date">30 November, 2018</span>
 
@@ -29,6 +31,11 @@
                 </div>
             @endforeach
             </div>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
             <div class="pagination">
                 <ul>
                     <li class="current"><a href="#">1</a></li>
