85dd0a129a351100eee1fdd3fedcc9f34501d9ed
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-10-23 06:21:36 +0530

    Admin Layout Updated

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -124,14 +124,14 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('admin/lib/chart/chart.min.js') }}"></script>
+    <script src="{{ asset('admin/lib/chart/chart.min.js') }}" defer></script>
     <script src="{{ asset('admin/lib/easing/easing.min.js') }}"></script>
     <script src="{{ asset('admin/lib/waypoints/waypoints.min.js') }}"></script>
     <script src="{{ asset('admin/lib/owlcarousel/assets/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('admin/lib/tempusdominus/js/moment.min.js') }}"></script>
     <script src="{{ asset('admin/lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
     <script src="{{ asset('admin/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-
+    
     <!-- Template Javascript -->
     <script src="{{ asset('admin/js/main.js') }}"></script>
     <script src="{{ asset('admin/js/custom.js') }}"></script>
