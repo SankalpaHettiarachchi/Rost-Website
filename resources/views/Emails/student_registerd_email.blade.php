9e58767fb0e786fb048afb4438cb70b28d53b18e
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-10-23 06:29:15 +0530

    Email Template Tested

--- resources/views/Emails/student_registerd_email.blade.php
+++ resources/views/Emails/student_registerd_email.blade.php
@@ -1,5 +1,197 @@
-Hello {{ $student->name }},
 
-You have been registerd as an Student. Please login using your email address:
-{{ $student->name }}<br>
-{{ $student->email }}
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
+<meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS -->
+<meta name="format-detection" content="date=no"> <!-- disable auto date linking in iOS -->
+<meta name="format-detection" content="address=no"> <!-- disable auto address linking in iOS -->
+<meta name="format-detection" content="email=no"> <!-- disable auto email linking in iOS -->
+<title></title>
+
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+<style type="text/css">
+/*Basics*/
+body {margin:0px !important; padding:0px !important; display:block !important; min-width:100% !important; width:100% !important; -webkit-text-size-adjust:none;}
+table {border-spacing:0; mso-table-lspace:0pt; mso-table-rspace:0pt;}
+table td {border-collapse: collapse;mso-line-height-rule:exactly;}
+td img {-ms-interpolation-mode:bicubic; width:auto; max-width:auto; height:auto; margin:auto; display:block!important; border:0px;}
+td p {margin:0; padding:0;}
+td div {margin:0; padding:0;}
+td a {text-decoration:none; color: inherit;}
+/*Outlook*/
+.ExternalClass {width: 100%;}
+.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div {line-height:inherit;}
+.ReadMsgBody {width:100%; background-color: #ffffff;}
+/* iOS BLUE LINKS */
+a[x-apple-data-detectors] {color:inherit !important; text-decoration:none !important; font-size:inherit !important; font-family:inherit !important; font-weight:inherit !important; line-height:inherit !important;} 
+/*Gmail blue links*/
+u + #body a {color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit;}
+/*Buttons fix*/
+.undoreset a, .undoreset a:hover {text-decoration:none !important;}
+.yshortcuts a {border-bottom:none !important;}
+.ios-footer a {color:#aaaaaa !important;text-decoration:none;}
+/*Responsive*/
+@media screen and (max-width: 799px) {
+  td.img-responsive img {width:100%!important;max-width: 100%!important;height: auto!important;margin: auto;}
+  table.row {width: 100%!important;max-width: 100%!important;}
+  table.center-float, td.center-float {float: none!important;}
+  td.center-text{text-align: center!important;}
+  td.container-padding {width: 100%!important;padding-left: 15px!important;padding-right: 15px!important;}
+  td.container-padding10 {width: 100%!important;padding-left: 10px!important;padding-right: 10px!important;}
+  table.hide-mobile {display: none!important;}
+  tr.hide-mobile {display: none!important;}
+  td.hide-mobile {display: none!important;}
+  br.hide-mobile {display: none!important;}
+  td.menu-container {text-align: center !important;}
+  td.autoheight {height: auto!important;}
+  table.m-padding-10 {margin: 10px 0!important;}
+  table.m-padding-15 {margin: 15px 0!important;}
+  table.m-padding-20 {margin: 20px 0!important;}
+  table.m-padding-30 {margin: 30px 0!important;}
+  table.m-padding-40 {margin: 40px 0!important;}
+  table.m-padding-50 {margin: 50px 0!important;}
+  table.m-padding-60 {margin: 60px 0!important;}
+  table.m-padding-top10 {margin: 30px 0 0 0!important;}
+  table.m-padding-top15 {margin: 15px 0 0 0!important;}
+  table.m-padding-top20 {margin: 20px 0 0 0!important;}
+  table.m-padding-top30 {margin: 30px 0 0 0!important;}
+  table.m-padding-top40 {margin: 40px 0 0 0!important;}
+  table.m-padding-top50 {margin: 50px 0 0 0!important;}
+  table.m-padding-top60 {margin: 60px 0 0 0!important;}
+  td.m-height10 {font-size:10px!important;line-height:10px!important;height:10px!important;}
+  td.m-height15 {font-size:15px!important;line-height:15px!important;height:15px!important;}
+  td.m-height20 {font-size:20px!important;line-height:20px!important;height:20px!important;}
+  td.m-height25 {font-size:25px!important;line-height:25px!important;height:25px!important;}
+  td.m-height30 {font-size:30px!important;line-height:30px!important;height:30px!important;}
+  td.rwd-on-mobile {display: inline-block!important;padding: 5px;}
+  tr.center-on-mobile {text-align: center!important;}
+}
+</style>
+<style type="text/css" class="export-delete">
+  .composer--mobile td.img-responsive img {width:100%!important;max-width: 100%!important;height: auto!important;margin: auto;}
+  .composer--mobile table.row {width: 100%!important;max-width: 100%!important;}
+  .composer--mobile table.center-float, td.center-float {float: none!important;}
+  .composer--mobile td.center-text{text-align: center!important;}
+  .composer--mobile td.container-padding {width: 100%!important;padding-left: 15px!important;padding-right: 15px!important;}
+  .composer--mobile table.hide-mobile {display: none!important;}
+  .composer--mobile tr.hide-mobile {display: none!important;}
+  .composer--mobile td.hide-mobile {display: none!important;}
+  .composer--mobile br.hide-mobile {display: none!important;}
+  .composer--mobile td.menu-container {text-align: center !important;}
+  .composer--mobile td.autoheight {height: auto!important;}
+  .composer--mobile table.m-padding-10 {margin: 10px 0!important;}
+  .composer--mobile table.m-padding-15 {margin: 15px 0!important;}
+  .composer--mobile table.m-padding-20 {margin: 20px 0!important;}
+  .composer--mobile table.m-padding-30 {margin: 30px 0!important;}
+  .composer--mobile table.m-padding-40 {margin: 40px 0!important;}
+  .composer--mobile table.m-padding-50 {margin: 50px 0!important;}
+  .composer--mobile table.m-padding-60 {margin: 60px 0!important;}
+  .composer--mobile table.m-padding-top10 {margin: 30px 0 0 0!important;}
+  .composer--mobile table.m-padding-top15 {margin: 15px 0 0 0!important;}
+  .composer--mobile table.m-padding-top20 {margin: 20px 0 0 0!important;}
+  .composer--mobile table.m-padding-top30 {margin: 30px 0 0 0!important;}
+  .composer--mobile table.m-padding-top40 {margin: 40px 0 0 0!important;}
+  .composer--mobile table.m-padding-top50 {margin: 50px 0 0 0!important;}
+  .composer--mobile table.m-padding-top60 {margin: 60px 0 0 0!important;}
+  .composer--mobile td.m-height10 {font-size:10px!important;line-height:10px!important;height:10px!important;}
+  .composer--mobile td.m-height15 {font-size:15px!important;line-height:15px!important;height:15px!important;}
+  .composer--mobile td.m-height20 {font-size:20px!important;line-height:20px!important;height:20px!important;}
+  .composer--mobile td.m-height25 {font-size:25px!important;line-height:25px!important;height:25px!important;}
+  .composer--mobile td.m-height30 {font-size:30px!important;line-height:30px!important;height:30px!important;}
+  .composer--mobile td.rwd-on-mobile {display: inline-block!important;padding: 5px;}
+  .composer--mobile tr.center-on-mobile {text-align: center!important;}
+</style>
+</head>
+
+<body  style="margin-top: 0; margin-bottom: 0; padding-top: 0; padding-bottom: 0; width: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;" bgcolor="#F4F4F4">
+
+<table   border="0" align="center" cellpadding="0" cellspacing="0" role="presentation" class="row" width="600" style="width:600px;max-width:600px;">
+  <tr>
+    <td align="center"  bgcolor="#e8b406" class="container-padding">
+      <table border="0" align="center" cellpadding="0" cellspacing="0" role="presentation" class="row" width="520" style="width:520px;max-width:520px;">
+        <tr >
+          <td height="40" style="font-size:40px;line-height:40px;" >&nbsp;</td>
+        </tr>
+        <tr  >
+          <td class="center-text"  align="center" style="font-family:'Playfair Display',Arial,Helvetica,sans-serif;font-size:30px;line-height:32px;font-weight:700;font-style:normal;color:#000000;text-decoration:none;letter-spacing:0px;">
+            
+              <div>
+                Hello {{ $student->name }}<br>
+                {{ $student->email }},
+              </div>
+            
+          </td>
+        </tr>
+        <tr >
+          <td height="25" style="font-size:25px;line-height:25px;" >&nbsp;</td>
+        </tr>
+        <tr  >
+          <td class="center-text"  align="center" style="font-family:'Poppins',Arial,Helvetica,sans-serif;font-size:14px;line-height:24px;font-weight:400;font-style:normal;color:#000000;text-decoration:none;letter-spacing:0px;">
+            
+              <div  >
+                You have been registerd as an Student
+              </div>
+            
+          </td>
+        </tr>
+        <tr >
+          <td height="15" style="font-size:15px;line-height:15px;" >&nbsp;</td>
+        </tr>
+        <tr  >
+          <td valign="middle" align="center" class="center-text">
+            <span    style="font-family:'Poppins',Arial,Helvetica,sans-serif;font-size:16px;line-height:30px;font-weight:300;font-style:normal;color:#000000;text-decoration:none;letter-spacing:0px;display:inline-block;vertical-align:middle;">
+                
+                  <div  >
+                    Rost Web
+                  </div>
+                
+            </span>
+            <span style="font-family:'Poppins',Arial,Helvetica,sans-serif;font-size:14px;line-height:30px;font-weight:300;font-style:normal;color:#796e49;text-decoration:line-through;letter-spacing:0px;display:inline-block;vertical-align:middle;"> 
+                
+                  <div  >
+                    Rost Web
+                  </div>
+                
+            </span>
+          </td>
+        </tr>
+        <tr >
+          <td height="20" style="font-size:20px;line-height:20px;" >&nbsp;</td>
+        </tr>
+        <tr  >
+          <td align="center">
+            <table border="0" cellspacing="0" cellpadding="0" role="presentation" align="center" class="center-float">
+              <tr>
+                <td align="center"    bgcolor="#FFFFFF" style="border-radius: 0px;">
+                      <a href="http://127.0.0.1:8000" target="_blank"    style="font-family:'Poppins',Arial,Helvetica,sans-serif;font-size:14px;line-height:14px;font-weight:400;font-style:normal;color:#000000;text-decoration:none;letter-spacing:0px;padding: 20px 43px 20px 43px;display: inline-block;"><span>SHOP NOW</span></a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr >
+          <td height="50" style="font-size:50px;line-height:50px;" >&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  </table>
+
+      </td>
+    </tr>
+    <tr>
+      <td height="50" class="m-height30" style="font-size:50px;line-height:50px;" >&nbsp;</td>
+    </tr>
+  </table>
+
+      </td>
+    </tr>
+</table>
+
+</body>
+</html>
