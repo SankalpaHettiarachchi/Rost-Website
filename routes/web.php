baff31f4fc8b1fb065ba0764fc08790d1100d8af
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-10-25 13:35:31 +0530

    footer updated

--- routes/web.php
+++ routes/web.php
@@ -9,18 +9,6 @@ use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 // ---------------AdminRoutes---------------
 
 Auth::routes();
