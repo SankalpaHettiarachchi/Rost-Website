ed85930b62d2d69dd4db043e5da2ce745d4513de
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-11-01 21:31:34 +0530

    Admin Reg view done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -70,6 +70,56 @@
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+<div class="container-fluid">
+    <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+        <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+            <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <a href="index.html" class="">
+                        <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>ROST</h3>
+                    </a>
+                    <h3>Sign Up</h3>
+                </div>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="form-floating mb-3">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="jhondoe">
+                        <label for="username">Username</label>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>      
+                    <div class="form-floating mb-3">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  required autocomplete="email" placeholder="name@example.com">
+                        <label for="email">Email address</label>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror                                       
+                    </div>
+                    <div class="form-floating mb-4">
+                        <input id="password"  type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required autocomplete="new-password">
+                        <label for="password">Password</label>
+                    </div>
+                    <div class="form-floating mb-4">
+                        <input type="password" class="form-control" id="password-confirm" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+                        <label for="confirm_password">Confirm Password</label>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror                    
+                    </div>
+                    <button type="submit" class="btn btn-primary py-3 w-100 mb-4">{{ __('Request') }}</button>
+                    <p class="text-center mb-0">Already have an Admin Account? <a href="">Sign In</a></p>
+                </form>
             </div>
         </div>
     </div>
