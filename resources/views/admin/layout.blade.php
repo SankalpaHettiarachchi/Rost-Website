34c81aab9a7d832b35a8cdf47d0ddffdb0607cdf
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-11-06 02:22:23 +0530

    Lay out updated

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -45,6 +45,7 @@
 </head>
 
 <body>
+
     <div class="container-fluid position-relative d-flex p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
@@ -126,11 +127,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>                               
             <!-- Footer End -->
+
         </div>
         <!-- Content End -->
-
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
